d032d4e5e4167efbff971220b40f4df7df4c2777
Pavel Kassitchev <pavkascool@gmail.com>
2021-06-17 07:59:05 +0300

    add view port

--- app/src/main/java/by/pavka/memento/Chronicler.java
+++ app/src/main/java/by/pavka/memento/Chronicler.java
@@ -1,15 +1,28 @@
 package by.pavka.memento;
 
+import com.jjoe64.graphview.series.DataPoint;
+
 import java.util.Calendar;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Chronicler {
     private Map<Calendar, Double> chronicle;
     private Calendar latestDate;
 
     public Chronicler() {
-        chronicle = new HashMap<Calendar, Double>();
+        chronicle = new TreeMap<Calendar, Double>(new Comparator<Calendar>() {
+            @Override
+            public int compare(Calendar o1, Calendar o2) {
+                if (o1.getTimeInMillis() > o2.getTimeInMillis()) {
+                    return 1;
+                }
+                return -1;
+            }
+        });
     }
 
     public boolean addRecord(Calendar calendar, double weight) {
@@ -23,6 +36,28 @@ public class Chronicler {
             return true;
         }
         return false;
+    }
+
+    public DataPoint[] getDataSeries() {
+        DataPoint[] dataSeries = new DataPoint[chronicle.size()];
+        TreeMap<Calendar, Double> copy = new TreeMap<>(chronicle);
+        for (int i = 0; i < dataSeries.length; i++) {
+            Map.Entry<Calendar, Double> entry = copy.pollFirstEntry();
+            Date date = entry.getKey().getTime();
+            double weight = entry.getValue();
+            dataSeries[i] = new DataPoint(date, weight);
+        }
+        return dataSeries;
+    }
+
+
+    public int getTimeIndex(Calendar calendar) {
+        TreeMap<Calendar, Double> copy = new TreeMap<>(chronicle);
+        int index = 0;
+        while (copy.pollFirstEntry().getKey().before(calendar)) {
+            index++;
+        }
+        return index;
     }
 
 
--- app/src/main/java/by/pavka/memento/HistoryActivity.java
+++ app/src/main/java/by/pavka/memento/HistoryActivity.java
@@ -11,33 +11,43 @@ import android.widget.Button;
 import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Calendar;
 
 import by.pavka.memento.databinding.ActivityHistoryBinding;
 
 public class HistoryActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Chronicler chronicler;
+    private GraphView graphView;
+    private DataPoint[] data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        chronicler = ((MementoApplication)getApplication()).getUser().getChronicler();
         ActivityHistoryBinding binding = ActivityHistoryBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
         Spinner period = binding.period;
         period.setOnItemSelectedListener(this);
-        GraphView graphView = binding.graph;
+        graphView = binding.graph;
 
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-
-        graphView.addSeries(series);
+        data = chronicler.getDataSeries();
+        if (data.length > 0) {
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(data);
+            graphView.addSeries(series);
+            graphView.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this));
+            graphView.getGridLabelRenderer().setNumHorizontalLabels(3);
+            graphView.getViewport().setMaxX(data[data.length - 1].getX());
+            graphView.getViewport().setXAxisBoundsManual(true);
+            graphView.getGridLabelRenderer().setHumanRounding(false);
+            graphView.getGridLabelRenderer().setVerticalAxisTitle(getResources().getString(R.string.weight));
+        }
 
         BottomNavigationView bottomNavigationView = binding.bottomNavigation.getRoot();
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationListener(this));
@@ -47,10 +57,28 @@ public class HistoryActivity extends AppCompatActivity implements AdapterView.On
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Log.d("PERIOD", "???");
-        int choice = parent.getSelectedItemPosition();
-        if (choice > 1) {
-            Log.d("PERIOD", "!!!");
+        if (data.length > 0) {
+            int choice = parent.getSelectedItemPosition();
+            int index = 0;
+            Calendar start = Calendar.getInstance();
+            switch (choice) {
+                case 1:
+                    start.add(Calendar.MONTH, -6);
+                    index = chronicler.getTimeIndex(start);
+                    break;
+                case 2:
+                    start.add(Calendar.MONTH, -3);
+                    index = chronicler.getTimeIndex(start);
+                    break;
+                case 3:
+                    start.add(Calendar.MONTH, -1);
+                    index = chronicler.getTimeIndex(start);
+                    break;
+                default:
+                    break;
+            }
+            graphView.getViewport().setMinX(data[index].getX());
+            graphView.onDataChanged(false, false);
         }
     }
 
